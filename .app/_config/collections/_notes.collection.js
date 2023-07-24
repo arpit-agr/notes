9b57926973f69a3ac5b2439b8871b8f3ce60beb4
arpit-agr <arpit.agrawal2502@gmail.com>
2023-07-24 18:21:54 +0530

    change  _notes collection's sorting to use the default 11ty sort: ascending by date

--- .app/_config/collections/_notes.collection.js
+++ .app/_config/collections/_notes.collection.js
@@ -1,7 +1,3 @@
 module.exports = () => (collectionApi) => {
-  return collectionApi.getFilteredByGlob("../**/*.md").sort((a, b) => {
-    const nameA = a.data.title || a.fileSlug;
-    const nameB = b.data.title || b.fileSlug;
-    return nameA.localeCompare(nameB);
-  });
+	return collectionApi.getFilteredByGlob("../**/*.md");
 };
